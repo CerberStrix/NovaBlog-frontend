f70c4084a67e3bdc0b99be52826c40d3790e715c
Alisher Kim <cerberstrix@gmail.com>
2023-03-13 04:17:09 +0500

    initail frontend

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes } from 'react-router-dom';
+import Container from "@mui/material/Container";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          {/* <Home /> */}
+        {/* <FullPost /> */}
+        {/* <AddPost /> */}
+        {/* <Login /> */}
+        {/* <Registration /> */}
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
